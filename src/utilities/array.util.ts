7ae4088867e5989765efea094c369f35d97f54a2
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-08-28 22:12:01 +0330

    feat: add chunk util

---
+++ src/utilities/array.util.ts
@@ -0,0 +1,13 @@
+export function toChunks<T>(items: T[], size: number): T[][] {
+  if (size <= 0) {
+    throw new Error("Chunk size must be greater than zero");
+  }
+
+  const chunks: T[][] = []
+  const chunkLength = Math.ceil(items.length / size)
+  for (let index = 0; index < chunkLength; index++) {
+    const chunk = items.slice(index * size, size * (index + 1))
+    chunks.push(chunk)
+  }
+  return chunks
+}
