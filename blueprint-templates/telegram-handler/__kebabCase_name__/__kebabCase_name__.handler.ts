c3cf5ea82a37d6b5e87e4563cf9f3549440a73b9
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-08-31 02:40:37 +0330

    chore: update telegram-hand;er blueprint-template

--- blueprint-templates/telegram-handler/__kebabCase_name__/__kebabCase_name__.handler.ts
+++ blueprint-templates/telegram-handler/__kebabCase_name__/__kebabCase_name__.handler.ts
@@ -1,6 +1,6 @@
 import { Context } from "grammy"
 import { I{{pascalCase name}}Handler } from './{{kebabCase name}}.model'
 
-export class {{pascalCase name}} implements I{{pascalCase name}}Handler {
+export class {{pascalCase name}}Handler implements I{{pascalCase name}}Handler {
   async handle(context: Context): Promise<void> { }
 }
