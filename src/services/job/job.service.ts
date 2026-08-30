ee180bfa315d36263f7bc97dc9bb4d76364a2f51
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-08-30 18:08:53 +0330

    fix: elliminate async/await on getJobs

--- src/services/job/job.service.ts
+++ src/services/job/job.service.ts
@@ -27,8 +27,8 @@ export class JobService implements IJobService {
     return result
   }
 
-  async getJobs(page: number, limit: number): Promise<IJob[]> {
-    return await this.repository.findPaginated(page, limit)
+  getJobs(page: number, limit: number): Promise<IJob[]> {
+    return this.repository.findPaginated(page, limit)
   }
 
   isValid(data: ICrawledJob): boolean {
