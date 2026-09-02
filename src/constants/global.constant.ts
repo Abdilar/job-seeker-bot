82d50c5472a8529f81c902c844c228f0d258fd27
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-09-02 20:51:46 +0330

    feat: add PROVIDER_MAP in global constant

--- src/constants/global.constant.ts
+++ src/constants/global.constant.ts
@@ -1,8 +1,20 @@
-import { EContractType } from "../types"
+import { EContractType, EProvider } from "../types"
 
 export const PAGINATION_LIMIT = 15
 
 export const CONTRACT_TYPE_MAP = {
   [EContractType.FULL_TIME]: 'تمام وقت',
   [EContractType.PART_TIME]: 'پاره وقت'
+}
+
+export const PROVIDER_MAP = {
+  [EProvider.JOB_IN_JA]: 'جابینجا',
+  [EProvider.IRAN_TALENT]: 'ایران تلنت',
+  [EProvider.JOB_VISION]: 'جاب ویژن',
+  [EProvider.GREENHOUSE]: 'Greenhouse',
+  [EProvider.INDEED]: 'Indeed',
+  [EProvider.LEVER]: 'Lever',
+  [EProvider.LINKED_IN]: 'Linkedin',
+  [EProvider.SEEK]: 'Seek',
+  [EProvider.WORKDAY]: 'Workday',
 }
