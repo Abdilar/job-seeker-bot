aadc964eb113ce8f5bd4f4546ac800ba7ff56658
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-09-05 02:25:22 +0330

    fix: update bot type in IStartHandler register method

--- src/telegram/handlers/start/start.model.ts
+++ src/telegram/handlers/start/start.model.ts
@@ -1,5 +1,6 @@
 import { Bot } from "grammy";
+import { TelegramContextType } from "../../telegram.model";
 
 export interface IStartHandler {
-  register(bot: Bot): void
+  register(bot: Bot<TelegramContextType>): void
 }
