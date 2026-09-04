139d83c5873724173c9790bb47fd558447cfe5cd
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-09-05 02:18:01 +0330

    feat: add ITelegramSession and TelegramContextType

--- src/telegram/telegram.model.ts
+++ src/telegram/telegram.model.ts
@@ -1,3 +1,12 @@
+import { Context, SessionFlavor } from "grammy"
+import { IJobFilter } from "../types"
+
 export interface ITelegramBot {
   start(): void
 }
+
+export interface ITelegramSession {
+  jobFilter: IJobFilter
+}
+
+export type TelegramContextType = Context & SessionFlavor<ITelegramSession>
