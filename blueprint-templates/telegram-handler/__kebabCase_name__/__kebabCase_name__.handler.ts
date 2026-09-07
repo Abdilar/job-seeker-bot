6d7e13bd69829a67f1936470515abdb05a3483e8
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-09-07 20:32:24 +0330

    chore: update telegram handler template

--- blueprint-templates/telegram-handler/__kebabCase_name__/__kebabCase_name__.handler.ts
+++ blueprint-templates/telegram-handler/__kebabCase_name__/__kebabCase_name__.handler.ts
@@ -1,6 +1,7 @@
 import { Bot } from "grammy"
 import { I{{pascalCase name}}Handler } from './{{kebabCase name}}.model'
+import { TelegramContextType } from "../../telegram.model"
 
 export class {{pascalCase name}}Handler implements I{{pascalCase name}}Handler {
-  register(bot: Bot): void { }
+  register(bot: Bot<TelegramContextType>): void { }
 }
