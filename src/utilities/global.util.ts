1320c1513d2a9cb2dbd9d1ba10ffaa5d8df0fe48
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-09-10 23:55:18 +0330

    feat: add randomDelay util

---
+++ src/utilities/global.util.ts
@@ -0,0 +1,11 @@
+export const randomDelay = async (
+  min: number,
+  max: number,
+): Promise<void> => {
+  const delay =
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  await new Promise((resolve) =>
+    setTimeout(resolve, delay),
+  );
+};
