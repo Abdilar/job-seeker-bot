5015cfa9ea5dd790f2905181158e503745d75e35
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2026-09-15 19:07:52 +0330

    chore(release.config): format the configs

--- release.config.mjs
+++ release.config.mjs
@@ -1,8 +1,8 @@
 export default {
   branches: ['master'],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-  ]
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
 }
