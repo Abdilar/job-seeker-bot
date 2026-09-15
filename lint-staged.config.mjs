5c7993a238111ded2eb1c1e5d45ee828a6e9bec6
Saeed Abdilar <saeed.abdilar@gmail.com>
2026-09-15 18:04:24 +0330

    Semantic release (#3)
    * chore(package): install semantic-release package
    
    * chore(package): add semanti release base config
    
    * chore(release): add semantic release steps
    
    * style(lint-staged.config): add checking root directory and format that if exist

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
   'src/**/*.{ts,js}': ['oxlint --fix', 'prettier --write'],
 
-  '*.{json,md,yml,yaml}': ['prettier --write'],
+  '**/*.{json,md,yml,yaml}': ['prettier --write'],
 }
