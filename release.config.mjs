ca3a9d6f706e465fbf919f8864fea56946251f43
Saeed Abdilar <saeed.abdilar@gmail.com>
2026-09-15 19:36:30 +0330

    Chore/semantic release (#8)
    * chore(package): install semantic-release package
    
    * chore(package): add semanti release base config
    
    * chore(release): add semantic release steps
    
    * style(lint-staged.config): add checking root directory and format that if exist
    
    * chore(release): configure semantic release plugins
    
    * chore(release.config): format the configs
    
    * chore(release): add release rules for semantic release
    
    * chore(lint-staged): update rules

--- release.config.mjs
+++ release.config.mjs
@@ -2,6 +2,12 @@ export default {
   branches: ['master'],
   plugins: [
     '@semantic-release/commit-analyzer',
+    {
+      releaseRules: [
+        { type: 'refactor', release: 'patch' },
+        { type: 'pref', release: 'patch' },
+      ],
+    },
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
   ],
